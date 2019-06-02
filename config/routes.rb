f985605307326ef6248c3c0513cd1bd0fdbb57f3
Noah Flint <noah.flint24@gmail.com>
2019-06-02 17:25:25 -0600

    created namespace route for get /forecast

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/api/v1/forecast', to: 'forcast#show'
+
+  namespace :api do
+    namespace :v1 do
+      get '/forecast', to: 'forecast#show'
+    end
+  end
 end
