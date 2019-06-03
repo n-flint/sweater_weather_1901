dcbda6bac515580526d380be00a2a61908f1b179
Noah Flint <noah.flint24@gmail.com>
2019-06-03 16:09:00 -0600

    created background facade

---
+++ app/facades/background_facade.rb
@@ -0,0 +1,32 @@
+class BackgroundFacade
+  attr_reader :id
+
+  def initialize(location)
+    @id = 1
+    @location = location
+  end 
+
+  def background_image
+      response = Faraday.get('https://api.flickr.com/services/rest/') do |f|
+        f.params['api_key'] = ENV['FLICKR_KEY']
+        f.params['method'] = 'flickr.photos.search'
+        f.params['tags'] = 'denver,co,skyline,weather'
+        f.params['text'] = 'denver,co'
+        f.params['nojsoncallback'] = 1
+        # f.params['text'] = 'denver, co'
+        f.params['format'] = 'json'
+      end
+      flickr_data = JSON.parse(response.body)
+      photo_data = flickr_data['photos']['photo'][0]
+      background(photo_data)
+  end
+
+  def background(photo_data)
+    Background.new(photo_data)
+  end
+
+  def background_image_url
+
+  end
+
+end
