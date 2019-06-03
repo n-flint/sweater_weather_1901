c51d4035a5e8c50b354bab8ff28de894bf94b766
Noah Flint <noah.flint24@gmail.com>
2019-06-03 16:08:26 -0600

    refactored show action

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::BackgroundController < ApplicationController
   def show
     location = params[:location]
     background = BackgroundFacade.new(location)
-    # require 'pry'; binding.pry
     render json: BackgroundSerializer.new(background)
   end 
 
