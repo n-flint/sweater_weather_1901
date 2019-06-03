9af34df0cb8f30e1efda95933144de27900337ad
Noah Flint <noah.flint24@gmail.com>
2019-06-03 12:00:53 -0600

    created get_antipode_coordinates

--- app/services/antipode_service.rb
+++ app/services/antipode_service.rb
@@ -1,11 +1,15 @@
 class AntipodeService
 
-  def initialize(location)
-    @location = location
+  def initialize(coordinates)
+    @latitude = coordinates.first
+    @longitude = coordinates.second
   end
 
-  def get_location
-    response = Faraday.get()
+  def get_antipode_coordinates
+    response = Faraday.get("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{@latitude}&long=#{@longitude}") do |f|
+      f.headers['api_key'] = 'oscar_the_grouch'
+    end
+    JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
   end
 
 end
