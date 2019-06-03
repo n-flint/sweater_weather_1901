a91a99c34edddc43d4bf693d33f4d1e8f574e7cf
Noah Flint <noah.flint24@gmail.com>
2019-06-03 17:50:14 -0600

    created account creation request spec

---
+++ spec/requests/api/v1/account_creation_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Account creation', type: :request do
+  it 'can create a user and return an api key' do
+    post '/api/v1/users'
+
+    user_data = JSON.parse(response.body)
+
+    expect(response.code).to eq(201)
+    expect(user_data).to have_key(['api_key'])
+  end
+end
