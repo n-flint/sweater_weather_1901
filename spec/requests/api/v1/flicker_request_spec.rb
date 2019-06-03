c58a80e0c402540cf684e42b7da4e6c863543eec
Noah Flint <noah.flint24@gmail.com>
2019-06-03 16:10:15 -0600

    creatd background serializer

--- spec/requests/api/v1/flicker_request_spec.rb
+++ spec/requests/api/v1/flicker_request_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'Background image for city', type: :request do
   it 'can get flicker image based on city' do
     get '/api/v1/backgrounds?location=denver,co'
 
-    flicker_data = JSON.parse(repsone.body)
+    flicker_data = JSON.parse(response.body)
 
-    expect(respone.code).to eq('200')
+    expect(response.code).to eq('200')
   end
 end
