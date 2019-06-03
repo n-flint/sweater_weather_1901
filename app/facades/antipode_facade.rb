c2eaf6c6bf3b1cf9ba2e771cbae3a03bb67e4386
Noah Flint <noah.flint24@gmail.com>
2019-06-03 11:06:35 -0600

    added location_name method to facade

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -1,7 +1,16 @@
 class AntipodeFacade
+  attr_reader :id
 
   def initialize(location)
-    require 'pry'; binding.pry
+    @id = 1
+    @location = location
   end
+
+  def location_name
+    coordinates = GeocodeService.new(@location).get_lat_long
+    @_location_name = AntipodeService.new(coordinates).get_location
+  end
+
+  
 
 end
