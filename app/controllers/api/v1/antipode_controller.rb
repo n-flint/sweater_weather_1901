cf943b3f48cc793a4416138255f58f282d4bbe96
Noah Flint <noah.flint24@gmail.com>
2019-06-03 09:41:26 -0600

    added render json to show action

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::AntipodeController < ApplicationController
   def show
     location = params[:loc]
     antipode = AntipodeFacade.new(location)
+    render json: AntipodeSerializer.new(antipode)
   end
 end
