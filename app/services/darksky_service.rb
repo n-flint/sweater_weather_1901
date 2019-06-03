91fda7f9b06e70f34718619d616e6fdd26cb3070
Noah Flint <noah.flint24@gmail.com>
2019-06-02 18:18:22 -0600

    created darksky service

---
+++ app/services/darksky_service.rb
@@ -0,0 +1,20 @@
+class DarkskyService
+
+  def initialize(coordinates)
+    require 'pry'; binding.pry
+    @coordinates = coordinates
+    @latitude = coordinates.first
+    @longitude = coordinates.second
+  end
+
+  def get_weather
+    make_call
+  end
+
+  private
+
+  def make_call
+    response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARKSKY_API']}/#{@latitude}/#{@longitude}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
