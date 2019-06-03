f6be330c22e8b8b65da7dffed32eeb39bc2cf9f8
Noah Flint <noah.flint24@gmail.com>
2019-06-03 08:41:04 -0600

    added expectations to weather_request

--- spec/requests/api/v1/weather_request_spec.rb
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe 'Weather For City', type: :request do
   it 'can get weather by city and state' do
     get "/api/v1/forecast?location=denver,co"
 
-    expect(response).to eq(200)
-    # require 'pry'; binding.pry
-    
+    weather_data = JSON.parse(response.body)
+
+    expect(response.code).to eq('200')
+    expect(weather_data).to have_key('data')
+    expect(weather_data['data']).to have_key('attributes')
+    require 'pry'; binding.pry
   end
 end
