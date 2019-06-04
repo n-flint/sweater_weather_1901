c0bb90d6cfff19ebc1fabcd4fb28a0d37ce42a66
Noah Flint <noah.flint24@gmail.com>
2019-06-04 09:18:03 -0600

    added get roadtrip route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'background#show'
       post '/users', to: 'users#create'
+      get '/road_trip', to: 'roadtrip#show'
     end
   end
 end
