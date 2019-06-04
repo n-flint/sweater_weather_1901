9ff886e6dd92eed4ccdc778f2e5c500f4cbacfa4
Noah Flint <noah.flint24@gmail.com>
2019-06-03 20:14:26 -0600

    added json params to post request

--- spec/requests/api/v1/account_creation_spec.rb
+++ spec/requests/api/v1/account_creation_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Account creation', type: :request do
   it 'can create a user and return an api key' do
-    post '/api/v1/users'
+    post '/api/v1/users', params: { 'email': 'whatever@example.com', 'password': 'password', 'password_confirmation': 'password'}
 
     user_data = JSON.parse(response.body)
 
