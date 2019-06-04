ba8396b85e38e4d24b72dc858395ba2f3bc870cb
Noah Flint <noah.flint24@gmail.com>
2019-06-03 21:07:40 -0600

    added if logic to return status codes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,14 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    @user = User.new(user_params)
-    @user.api_key = SecureRandom.hex(10)
-    @user.save
-    user = UserFacade.new(@user)
-    render json: UserSerializer.new(user)
+    user = User.new(user_params)
+    user.api_key = SecureRandom.hex(10)
+    if user.save
+        user = UserFacade.new(user)
+        render json: UserSerializer.new(user), status: 201
+    else
+      render json:{}, status: 401
+    end
   end
 
 
