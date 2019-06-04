3aae6b79bd5226d286510ef13f87e317cf0bbb7c
Noah Flint <noah.flint24@gmail.com>
2019-06-04 11:25:15 -0600

    added more expectations to test

--- spec/requests/api/v1/roadtrip_spec.rb
+++ spec/requests/api/v1/roadtrip_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe 'Roadtrip Weather', type: :request do
     trip_data = JSON.parse(response.body)
 
     expect(response.code).to eq('200')
+
+    expect(trip_data['data'].class).to eq(Hash)
+    expect(trip_data['data']['id'].class).to eq(String)
+    expect(trip_data['data']['type']).to eq('roadtrip')
+    expect(trip_data['data']['attributes']['id'].class).to eq(Integer)
     expect(trip_data['data']['attributes']['temperature'].class).to eq(Float)
     expect(trip_data['data']['attributes']['summary'].class).to eq(String)
   end
