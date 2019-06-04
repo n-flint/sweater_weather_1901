aca7cc948ce72474827c466fd1fb9c65ac29f479
Noah Flint <noah.flint24@gmail.com>
2019-06-04 10:57:38 -0600

    mnade summary method to get summary from hour information

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -8,16 +8,30 @@ class RoadtripFacade
   end
 
   def travel_time
-    response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{@start}&destination=#{@finish}&key=AIzaSyDhDQjEpyB8yVM7XBI-kWwiiDDD1MQv34Y")
+    response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{@start}&destination=#{@finish}&key=#{ENV['GOOGLE_DIRECTIONS_KEY']}")
 
     data = JSON.parse(response.body)
+    
+    travel_time = data['routes'][0]['legs'][0]['duration']['value']
+    #takes the travel length then rounds it to the next hour, -1 to search by index value in next call
+    hour = (travel_time/3600.00).ceil - 1
+    # require 'pry'; binding.pry
+  end
+  
+  def destination_weather
+    offset = travel_time
+    response = Faraday.get("https://api.darksky.net/forecast/0eb5005ca21c3ac641d6adb0d54c7995/38.2542024,-104.6087502?exclude=currently,minutely,daily,flags")
+    
+    data = JSON.parse(response.body)
+    
+    hour_info = data['hourly']['data'][offset]
+  end
 
-    travel_time = data['routes'][0]['legs'][0]['duration']
+  def summary
+    destination_weather['summary']
   end
 
   def temperature
-    response = Faraday.get("https://api.darksky.net/forecast/0eb5005ca21c3ac641d6adb0d54c7995/38.2542024,-104.6087502?exclude=currently,minutely,daily,flags")
-    data = JSON.parse(response.body)
-    hour = data['hourly']['data'][0]
+    destination_weather['temperature']
   end
 end
