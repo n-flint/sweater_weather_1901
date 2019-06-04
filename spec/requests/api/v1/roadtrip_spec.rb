fabc485d6bc8b70fdaf91ad1edf34690faedd5db
Noah Flint <noah.flint24@gmail.com>
2019-06-04 10:57:08 -0600

    refactored test to expect certain classes

--- spec/requests/api/v1/roadtrip_spec.rb
+++ spec/requests/api/v1/roadtrip_spec.rb
@@ -4,24 +4,14 @@ RSpec.describe 'Roadtrip Weather', type: :request do
   it 'can get weather from roadtrip destination' do
     get '/api/v1/road_trip?start=denver,co&end=pueblo,co'
 
-    trip_data = JSON.parse
+    trip_data = JSON.parse(response.body)
 
-    expect(response.code).to eq(200)
-    expect(user_data['data']['attributes']).to have_key('temperature')
-    expect(user_data['data']['attributes']).to have_key('summary')
+    expect(response.code).to eq('200')
+    expect(trip_data['data']['attributes']['temperature'].class).to eq(Float)
+    expect(trip_data['data']['attributes']['summary'].class).to eq(String)
   end
 end
 
-# The object of this assessment is to create an API endpoint to assist in the planning of road trips.
-
-# You will create an endpoint like so:, `/api/v1/road_trip?start=denver,co&end=pueblo,co`
-
-# You will use the Google Directions API:  https://developers.google.com/maps/documentation/directions/start
-# in order to find out how long it will take to travel from the two locations, and then deliver the weather forecast for the hour
-# of arrival.
-
-# For example, Denver to Pueblo would take two hours. You will deliver a forecast two hours in the future that includes the temperature and summary.
-# Collapse
 
 
 
