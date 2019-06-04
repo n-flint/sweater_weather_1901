7dfb87f3cd2ddf79f58853ba4e10da9eb566d83e
Noah Flint <noah.flint24@gmail.com>
2019-06-04 10:26:33 -0600

    created darksky call with travel time

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -4,7 +4,20 @@ class RoadtripFacade
   def initialize(start, finish)
     @id = 1
     @start = start
-    @end = finish
+    @finish = finish
   end
 
+  def travel_time
+    response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{@start}&destination=#{@finish}&key=AIzaSyDhDQjEpyB8yVM7XBI-kWwiiDDD1MQv34Y")
+
+    data = JSON.parse(response.body)
+
+    travel_time = data['routes'][0]['legs'][0]['duration']
+  end
+
+  def temperature
+    response = Faraday.get("https://api.darksky.net/forecast/0eb5005ca21c3ac641d6adb0d54c7995/38.2542024,-104.6087502?exclude=currently,minutely,daily,flags")
+    data = JSON.parse(response.body)
+    hour = data['hourly']['data'][0]
+  end
 end
